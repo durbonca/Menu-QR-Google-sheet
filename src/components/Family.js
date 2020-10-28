51e8d7dbc4d6ba7ce126f5837f8d5f585af94eab
Manuel Duran <durbonca@gmail.com>
2020-10-28 19:04:40 -0300

    adding more stuff

--- src/components/Family.js
+++ src/components/Family.js
@@ -3,27 +3,28 @@ import dataProducts from '../data/data';
 import Product from './Product'
 import {Link} from 'react-router-dom'
 import Bars from "./bars";
+import Ostras from "./ostras"
+import PuntoCarne from "./puntoCarnes" 
 
-class Family extends React.Component {
-    
+class Family extends React.Component{
     render (){
       return  <div className="container-fluid offset-md-3 col-md-6 mt-5 mb-5 px-4">
                 <Link className="btn backButtom" to="/"><span className="emoji" role="img" aria-label={""}>🔙</span></Link>
+                <Ostras/>
                 {dataProducts.map(function (dataProducts){
-                    return <div>
+                    return  <div>
                                 <div className="row mt-5 d-flex align-items-center">                               
-                                    <Bars/>
+                                    <Bars/> 
                                     <h3 className="col-auto text-center">{dataProducts.title}</h3>
                                     <Bars/>
-                                    <p><i>{dataProducts.description}</i></p>
                                 </div>
+                                    <p><i>{dataProducts.description}</i></p>
                                 <Product product={dataProducts.products}/>
-                            </div>
+                            </div> 
                 })}
-
+                <PuntoCarne/>
               </div> 
     }
 }
-
 
 export default Family
---
+++ src/components/ostras.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Ostras extends React.Component{
+    render(){
+        return <div className="row d-flex justify-content-between">
+                    <div className="d-flex flex-column align-items-center col-6 px-0">
+                        <h1>Ostras</h1>
+                        <small>calibre</small>
+                        <strong>“Victoria”</strong>
+                    </div>
+                    <div className="d-flex flex-column align-items-center justify-content-end col-6 px-0">
+                        <div className="d-flex">
+                            <strong className="col mx-0 px-1">15</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">12.000</strong>
+                        </div>
+                        <div className="d-flex justify-">
+                          <strong className="col mx-0 px-1">30</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">21.000</strong>
+                        </div>
+                    </div>
+               </div>
+    }
+}
+export default Ostras
