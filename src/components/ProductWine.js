2a164377b755c3914d61ace7331fd76c1b873e2c
Manuel Duran <durbonca@gmail.com>
2020-11-10 00:35:13 -0300

    Update ProductWine.js

--- src/components/ProductWine.js
+++ src/components/ProductWine.js
@@ -4,11 +4,20 @@ class Family extends React.Component {
     render (props){
         return  <div>
                  {this.props.product.map(function (product){ 
+                    
+                    let cup;
+                    if (product.price){
+                        cup = <div className="col-2">🍷</div>
+                    }else{
+                        cup = <div className="col-2"></div>
+                    }
+
                     return  <div className="row d-flex mt-1 item">
                                 <p className="col-6 text-left m-0 p-0">{product.name} <small className="col-auto text-right mr-0 p-0">{product.country}</small></p>
                                 <div class="col-6 d-flex">
-                                 <p className="col-6 text-right mb-0 ml-2 p-0">{product.price}</p>
-                                <p className="col-6 text-right mb-0 ml-2 p-0">{product.price2}</p>
+                                 {cup}
+                                <p className="col-5 text-right mb-0 ml-2 p-0">{product.price}</p>
+                                <p className="col-5 text-right mb-0 ml-2 p-0">{product.price2}</p>
                                 </div>
                                 <small className="col-auto text-left">{product.description}</small>
                             </div>
