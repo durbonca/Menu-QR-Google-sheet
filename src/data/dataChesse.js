8656c1cd64b86342f7d1af7891a31089da3e91a0
Manuel Duran <durbonca@gmail.com>
2020-12-14 12:16:51 -0300

    Update dataChesse.js

--- src/data/dataChesse.js
+++ src/data/dataChesse.js
@@ -11,7 +11,7 @@ const dataCoffe =
             { id: 5, name: 'Comté', description: 'Vaca', country: '(Francia)', gr: '45', price: '4.500' },
             /* , { id: 3, name: 'Laguiole', description: 'Vaca', country: '(Francia)', gr: '45', price: '4.000'} */ 
             { id: 6, name: 'Roquefort', description: 'Oveja', country: '(Francia)', gr: '45', price: '3.500' },
-            /* , { id: 5, name: 'P’tit Basque', description: 'Oveja', country: '(Francia)', gr: '45', price: '3.500' } */
+            , { id: 5, name: 'P’tit Basque', description: 'Oveja', country: '(Francia)', gr: '45', price: '3.500' }
             { id: 7, name: 'Cremoso de Callaqui', description: 'Cabra', country: '(Chile)', gr: '45', price: '3.000' }, 
             /* { id: 8, name: 'Nuestra selección 3 Quesos', description: 'Brillat Sabarin, Beaufort, Roquefort.', price: '9.000' }, */
             /* { id: 9, name: 'Nuestra Selección 4 Quesos', description: 'Soumaintrain, Brillat Savarin, Beaufort, Saint Nectaire.', price: '12.000' }, */
