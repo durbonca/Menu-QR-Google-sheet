a69016ffe514e34ffae82662b649327d6d6e7d98
Manuel Duran <durbonca@gmail.com>
2021-01-15 10:16:21 -0300

    cogote y goujenete

--- src/data/bacoteo.js
+++ src/data/bacoteo.js
@@ -6,7 +6,7 @@ const dataBacoteo = [{
        /* { id: 3, name: 'Locos', description: 'Al Natural', price: '13.000' }, */ 
        { id: 4, name: 'Pulpo', description: 'a la Plancha', price: '13.000' },
        /* { id: 5, name: 'Machas “Meunière”', description: '', price: '14.000'}, */ 
-       { id: 5, name: 'Goujonettes', description: 'Lamina de Pescado Apanado', price: '7.000' }, 
+       /* { id: 5, name: 'Goujonettes', description: 'Lamina de Pescado Apanado', price: '7.000' }, */ 
        { id: 6, name: 'Terrine de Foie Gras', description: '“Mi - Cuit”', price: '7.000' }, 
        { id: 7, name: 'Tostadas de Foie Gras', description: '“Mi - Cuit”', price: '8.000' }, 
        { id: 8, name: 'Tostadas de Queso de Cabra', description: '', price: '8.000' }, 
