182dd06fe05887cc1856164d2c53ad343868be50
Manuel Duran <durbonca@gmail.com>
2021-01-16 08:48:55 -0300

    no hay merluza ni reineta

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,9 @@
-const dataProducts = [
-    {
+const dataProducts = [{
         title: 'Fantasías',
-        products: 
-        [
+        products: [
             /* { id: 1, name: 'Chupe de Locos',description: '', price: '12.000' }, */
             /* { id: 2,name: 'Cogote de Merluza', description: 'al Ajillo',price: '8.000' }, */
-            { id: 3,name: 'Foie Gras “Poêlé”', description: '',price: '14.000' },
+            { id: 3, name: 'Foie Gras “Poêlé”', description: '', price: '14.000' },
             { id: 4, name: 'Huevo Meurette', description: 'con Salsa de Vino y Tocino', price: '9.000' },
             { id: 5, name: 'Médula de Res', description: '4 Unidades', price: '9.000' },
             { id: 6, name: 'Bacotillas', description: '4 “Feuilletes” de Champiñon', price: '8.000' },
@@ -17,52 +15,47 @@ const dataProducts = [
     },
     {
         title: 'Cremas y Sopas',
-        products: 
-        [   
+        products: [
             /* {id: 1,name: 'Crema de Locos',description: 'con Crutones',price: '7.000'}, */
-            {id: 2, name: 'Caldo del Mar',description: 'con Pescado, Mariscos y Piure',price: '6.000' },
-            {id: 3, name: 'Sopa de Cebolla', description: '', price: '6.000' }
+            { id: 2, name: 'Caldo del Mar', description: 'con Pescado, Mariscos y Piure', price: '6.000' },
+            { id: 3, name: 'Sopa de Cebolla', description: '', price: '6.000' }
         ]
     },
     {
         title: 'Ensaladas',
         description: '“Con su Dressing”',
-        products: 
-        [
+        products: [
             { id: 1, name: 'Quinoa', description: 'con “Kale”, Tomate, Palta y Pepino', price: '8.000' },
-            {id: 2, name: 'Salmón', description: 'con Salmón Pochado, Marinado y Dressing César', price: '9.000'},
+            { id: 2, name: 'Salmón', description: 'con Salmón Pochado, Marinado y Dressing César', price: '9.000' },
             { id: 3, name: 'Quesos', description: 'con Nuestra Selección de 4 Quesos y Nueces', price: '9.000' },
-            { id: 4, name: 'Chèvre “Chaud”', description: 'con Tomates Deshidratados y Semillas',price: '9.000'},
+            { id: 4, name: 'Chèvre “Chaud”', description: 'con Tomates Deshidratados y Semillas', price: '9.000' },
             { id: 5, name: 'Canard', description: 'con Magret, Confit, Mollejas y Foie Gras', price: '10.000' }
         ]
     },
     {
         title: 'Pastas',
-        products: 
-        [
-            { id: 1, name: 'Los 33 Ravioles',description: 'con Queso de Cabra, Perejil y Ajo',price: '11.000'},
-            { id: 2, name: 'Ñoquis', description: 'con Salsa 4 Quesos y Nueces', price: '9.000'},
-            { id: 3,name: 'Lasaña del Huerto',description: 'con Zapallo Italiano, Berenjena, Champiñones, Espinaca, Tomate, Pimientos y Pestos', price: '9.000' }
+        products: [
+            { id: 1, name: 'Los 33 Ravioles', description: 'con Queso de Cabra, Perejil y Ajo', price: '11.000' },
+            { id: 2, name: 'Ñoquis', description: 'con Salsa 4 Quesos y Nueces', price: '9.000' },
+            { id: 3, name: 'Lasaña del Huerto', description: 'con Zapallo Italiano, Berenjena, Champiñones, Espinaca, Tomate, Pimientos y Pestos', price: '9.000' }
         ]
     },
     {
         title: 'Del Mar',
         description: '“Según la Pesca”',
-        products: 
-        [
+        products: [
             { id: 1, name: 'Creole del Mar', description: 'con Arroz', price: '13.000' },
             { id: 11, name: 'Corvina', description: 'en su salsa y Sémola', price: '14.000' },
-            { id: 2, name: 'Merluza Frita', description: 'con Base de Puerro y Tomate', price: '12.000' },
-            { id: 3,  name: 'Merluza a la Vasca',description: 'con Piperrada y Arroz', price: '12.000' },
+            /* { id: 2, name: 'Merluza Frita', description: 'con Base de Puerro y Tomate', price: '12.000' }, */
+            /* { id: 3,  name: 'Merluza a la Vasca',description: 'con Piperrada y Arroz', price: '12.000' }, */
             /* { id: 4, name: 'Reineta “Meunière”', description: 'con Piperrada y Aligot', price: '13.000' }, */
-            { id: 5, name: 'Salmón Sellado', description: 'con Gratín de Zapallo y Tomates Provenzal', price: '14.000'}
+            { id: 5, name: 'Salmón Sellado', description: 'con Gratín de Zapallo y Tomates Provenzal', price: '14.000' }
         ]
     },
     {
         title: 'Carnes',
-        products: 
-        [
-            { id: 1, name: 'Steak “Tartar”', description: 'Filete de Res “CRUDO” con Huevo, Ensalada Verde y Papas Fritas', price: '10.000'},
+        products: [
+            { id: 1, name: 'Steak “Tartar”', description: 'Filete de Res “CRUDO” con Huevo, Ensalada Verde y Papas Fritas', price: '10.000' },
             { id: 2, name: 'Aiguillette de Canard', description: 'con Salsa Pimienta Verdey Papas Fritas', price: '12.000' },
             { id: 3, name: 'Alitas de Canard', description: 'con Papas y Champiñones', price: '11.000' },
             { id: 4, name: 'Cassoulet', description: 'Confit de Canard, Saucisse y Alubias', price: '12.000' },
@@ -70,7 +63,7 @@ const dataProducts = [
             { id: 6, name: 'Boeuf Bourguignon', description: 'Sobre Papas Confitadas', price: '10.000' },
             { id: 7, name: 'Strogonoff de Filete', description: 'con Champiñones y Arroz', price: '12.000' },
             { id: 8, name: 'Medallón de filete', description: 'con Salsa Béarnaise y “Pont Neuf”', price: '14.000' },
-            { id: 9, name: 'Medallón de filete', description: 'al Natural, Sólo Lechuga', price: '12.000'}
+            { id: 9, name: 'Medallón de filete', description: 'al Natural, Sólo Lechuga', price: '12.000' }
         ]
     }
 ]
