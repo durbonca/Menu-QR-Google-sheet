1bf39f2cf10b830650924573f68dbeb537b2b5a8
Manuel Duran <durbonca@gmail.com>
2021-01-19 17:50:36 -0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Link} from "react-router-dom"
-import bike from '../icons/delivery.svg'
+/* import bike from '../icons/delivery.svg' */
 
 class Home extends React.Component{
     render(){
