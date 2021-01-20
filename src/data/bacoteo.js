ad535d2c8737a364cde329bd16c2c7281e71a4af
Manuel Duran <durbonca@gmail.com>
2021-01-20 18:49:23 -0300

    locura de erizos

--- src/data/bacoteo.js
+++ src/data/bacoteo.js
@@ -1,28 +1,29 @@
 const dataBacoteo = [{
-    title: 'Bacoteos', products: [
-       /* {id:0, name: "Plato Mixto Charcuteria y Queso", description: "Serrano, Chorizo, Brillat Savarin, Comte y Roquefort", price: "13.000"},*/
-       { id: 1, name: 'Tártaro de Salmón', description: 'al Jengibre', price: '9.000' }, 
-       { id: 2, name: 'Salmón al Eneldo', description: '', price: '7.000' },
-       { id: 21, name: 'Tostadas de Erizos', description: '', price: '9.000' },
-       { id: 22, name: 'Erizos Frescos', description: 'Porción de lenguas de erizos frescos, con su salsa verde.', price: '14.000' }, 
-       /* { id: 3, name: 'Locos', description: 'Al Natural', price: '13.000' }, */ 
-       { id: 4, name: 'Pulpo', description: 'a la Plancha', price: '13.000' },
-       { id: 5, name: 'Machas “Meunière”', description: '', price: '14.000'}, 
-       { id: 5, name: 'Goujonettes', description: 'Lamina de Pescado Apanado', price: '7.000' }, 
-       { id: 6, name: 'Terrine de Foie Gras', description: '“Mi - Cuit”', price: '7.000' }, 
-       { id: 7, name: 'Tostadas de Foie Gras', description: '“Mi - Cuit”', price: '8.000' }, 
-       { id: 8, name: 'Tostadas de Queso de Cabra', description: '', price: '8.000' }, 
-       { id: 9, name: 'Plato Rosbif', description: 'con Mostaza y Béarnaise', price: '7.000' }, 
-       { id: 10, name: 'Plato 3 Charcuterias', description: 'Serrano - Chorizo - Saucisson', price: '8.000' },
-       /* { id: 9, name: 'Jamón Ibérico', description: 'de Bellota', price: '15.000' }, */
-        { id: 11, name: 'Mixta de Charcuteria y Queso', description: 'Chorizo, Serrano Y Selección de 3 Quesos', price: '14.000' }, 
-        { id: 12, name: 'Camembert Apanado', description: 'Y su reducción de Vino', price: '7.000' }, 
+    title: 'Bacoteos',
+    products: [
+        /* {id:0, name: "Plato Mixto Charcuteria y Queso", description: "Serrano, Chorizo, Brillat Savarin, Comte y Roquefort", price: "13.000"},*/
+        { id: 1, name: 'Tártaro de Salmón', description: 'al Jengibre', price: '9.000' },
+        { id: 2, name: 'Salmón al Eneldo', description: '', price: '7.000' },
+        { id: 22, name: 'Erizos a Natural', description: 'con su salsa verde.', price: '14.000' },
+        { id: 21, name: 'Tostadas de Erizos', description: '', price: '9.000' },
+        { id: 23, name: 'Omellette de Erizos', description: '', price: '9.000' },
+        /* { id: 3, name: 'Locos', description: 'Al Natural', price: '13.000' }, */
+        { id: 4, name: 'Pulpo', description: 'a la Plancha', price: '13.000' },
+        { id: 5, name: 'Machas “Meunière”', description: '', price: '14.000' },
+        { id: 5, name: 'Goujonettes', description: 'Lamina de Pescado Apanado', price: '7.000' },
+        { id: 6, name: 'Terrine de Foie Gras', description: '“Mi - Cuit”', price: '7.000' },
+        { id: 7, name: 'Tostadas de Foie Gras', description: '“Mi - Cuit”', price: '8.000' },
+        { id: 8, name: 'Tostadas de Queso de Cabra', description: '', price: '8.000' },
+        { id: 9, name: 'Plato Rosbif', description: 'con Mostaza y Béarnaise', price: '7.000' },
+        { id: 10, name: 'Plato 3 Charcuterias', description: 'Serrano - Chorizo - Saucisson', price: '8.000' },
+        /* { id: 9, name: 'Jamón Ibérico', description: 'de Bellota', price: '15.000' }, */
+        { id: 11, name: 'Mixta de Charcuteria y Queso', description: 'Chorizo, Serrano Y Selección de 3 Quesos', price: '14.000' },
+        { id: 12, name: 'Camembert Apanado', description: 'Y su reducción de Vino', price: '7.000' },
         /* { id: 13, name: 'Nuestra selección 3 Quesos', description: 'Brillat Sabarin, Beaufort, Roquefort.', price: '9.000' }, */
-        /* { id: 14, name: 'Nuestra Selección 4 Quesos', description: 'Soumaintrain, Brillat Savarin, Beaufort, Saint Nectaire.', price: '12.000' }, */ 
+        /* { id: 14, name: 'Nuestra Selección 4 Quesos', description: 'Soumaintrain, Brillat Savarin, Beaufort, Saint Nectaire.', price: '12.000' }, */
         /* { id: 11, name: 'Ostras', description: '15 Unidades', price: '12.000'}, */
         /* { id: 12, name: 'Ostras', description: '30 Unidades', price: '21.000' }, */
     ]
-}
-]
+}]
 
 export default dataBacoteo
