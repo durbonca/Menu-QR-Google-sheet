fb5eb90292cb91686c5a29abceb4cc7f6c475e6a
Manuel Duran <durbonca@gmail.com>
2021-01-28 12:56:32 -0300

    cambio tabla queso y sacar cogote

--- src/data/DataTablaqueso.js
+++ src/data/DataTablaqueso.js
@@ -3,7 +3,7 @@ const DataTablaqueso = [{
         {
             id: 1, name: 'Nuestra selección 3 Quesos', description: 'Brillat Sabarin, Beaufort, Roquefort.', price: '9.000'
         }, {
-            id: 2, name: 'Nuestra Selección 4 Quesos', description: 'Soumaintrain, Brillat Savarin, Beaufort, Roquefort.', price: '12.000'
+            id: 2, name: 'Nuestra Selección 4 Quesos', description: 'Buches, Brillat Savarin, Beaufort, Roquefort.', price: '12.000'
         }
     ]
 }]
