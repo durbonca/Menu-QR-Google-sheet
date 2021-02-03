71393c2d0cbc77aaf3efad7d2bcf0def63c8f2bb
Manuel Duran <durbonca@gmail.com>
2021-02-03 12:20:24 -0300

    Update dataWine.js

--- src/data/dataWine.js
+++ src/data/dataWine.js
@@ -54,7 +54,7 @@ const dataWine = [
             { id: 6, name: 'Larkün', description: 'de Pandolfi Price 2018', country: 'Itata', price: '', price2: '17.000' }, 
             { id: 7, name: 'Little quino', description: 'de William Fèvre 2019', country: 'Malleco', price: '', price2: '16.000' }, 
             { id: 8, name: 'Marina', description: 'de P.S García 2016', country: 'Casablanca', price: '', price2: '16.000'}, 
-            { id: 9, name: 'Clos Andino', description: 'Serie Limiteé 2017', country: 'Curico', price: '', price2: '15.000' }     
+            /* { id: 9, name: 'Clos Andino', description: 'Serie Limiteé 2017', country: 'Curico', price: '', price2: '15.000' } */     
         ]
     },{ 
         title: 'Sauvignon Gris', 
