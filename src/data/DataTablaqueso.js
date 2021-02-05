3c7a384526d06916c235c08b6ebbc7a9874e8606
Manuel Duran <durbonca@gmail.com>
2021-02-05 09:14:43 -0300

    Update DataTablaqueso.js

--- src/data/DataTablaqueso.js
+++ src/data/DataTablaqueso.js
@@ -1,7 +1,7 @@
 const DataTablaqueso = [{
     products: [
         {
-            id: 1, name: 'Nuestra selección 3 Quesos', description: 'Brillat Sabarin, Beaufort, Roquefort.', price: '9.000'
+            id: 1, name: 'Nuestra selección 3 Quesos', description: 'Brillat Savarin, Beaufort, Roquefort.', price: '9.000'
         }, {
             id: 2, name: 'Nuestra Selección 4 Quesos', description: 'Buches, Brillat Savarin, Beaufort, Roquefort.', price: '12.000'
         }
