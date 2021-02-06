75a28be68c2503430d6efc1ed833eb419c68e03b
Manuel Duran <durbonca@gmail.com>
2021-02-06 19:51:28 -0300

    Update dataDestilados.js

--- src/data/dataDestilados.js
+++ src/data/dataDestilados.js
@@ -56,11 +56,11 @@ const dataDestilados = [
                 [
                     { id: 1, name: 'Dewar’s', description: '18 Años', cl: 'Highland 6', price: '6.500'},
                     /* { id: 2, name: 'Chivas Regal', description: '18 Años', cl: 'Islay 6', price: '7.000'} */ ]}, 
-                    { 
+                    /* { 
                 title:'Japones', 
                 products: 
                 [
-                    { id: 1, name: 'Akashi Black', description: '', cl: 'Hyogo 6', price: '6.500'} ]}, 
+                    { id: 1, name: 'Akashi Black', description: '', cl: 'Hyogo 6', price: '6.500'} ]}, */ 
                 { 
                 title:'Whisky de Tennessee', 
                 products: 
