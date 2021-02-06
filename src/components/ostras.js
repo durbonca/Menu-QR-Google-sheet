2e0c3423245e3f76fa1b6361de3fd0c1b0ef6fba
Manuel Duran <durbonca@gmail.com>
2021-02-06 14:09:47 -0300

    Update ostras.js

--- src/components/ostras.js
+++ src/components/ostras.js
@@ -10,10 +10,10 @@ class Ostras extends React.Component{
                     </div>
                     <div className="d-flex flex-column align-items-center justify-content-end col-6 px-0">
                         <div className="d-flex">
-                            <strong className="col mx-0 px-1">15</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">12.000</strong>
+                            <strong className="col mx-0 px-1">12</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">12.000</strong>
                         </div>
                         <div className="d-flex justify-">
-                          <strong className="col mx-0 px-1">30</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">21.000</strong>
+                          <strong className="col mx-0 px-1">24</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">21.000</strong>
                         </div>
                     </div>
                </div>
