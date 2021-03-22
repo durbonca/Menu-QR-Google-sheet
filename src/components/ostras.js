807666734f7ad5e90b43bb98af698728244dc0dc
Manuel Duran <durbonca@gmail.com>
2021-03-22 18:46:09 -0300

    Update ostras.js

--- src/components/ostras.js
+++ src/components/ostras.js
@@ -4,7 +4,7 @@ import Bars from "./bars"
 class Ostras extends React.Component{
     render(){
         return <div className="container-fluid offset-md-3 col-md-6 mt-5 mb-5 px-4">
-                    <div className="row d-flex align-items-center">                               
+                {/*     <div className="row d-flex align-items-center">                               
                                     <Bars/> 
                                     <h3 className="col-auto text-center">Ostras</h3>
                                     <Bars/>
@@ -22,7 +22,7 @@ class Ostras extends React.Component{
                           <strong className="col mx-0 px-1">30</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">21.000</strong>
                         </div>
                     </div>
-               </div>
+               </div> */}
                
                {/* <div className="row mt-3 d-flex justify-content-between">
                     <div className="d-flex flex-column align-items-center col-6 px-0">
