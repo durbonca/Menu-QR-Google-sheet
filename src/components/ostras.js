88e334cf9ec150c696c2178551c252341e4b9378
Manuel Duran <durbonca@gmail.com>
2021-03-26 19:34:49 -0300

    Update ostras.js

--- src/components/ostras.js
+++ src/components/ostras.js
@@ -9,7 +9,7 @@ class Ostras extends React.Component{
                                     <h3 className="col-auto text-center">Ostras</h3>
                                     <Bars/>
                     </div>
-                <div className="row d-flex justify-content-between">
+                {/* <div className="row d-flex justify-content-between">
                     <div className="d-flex flex-column align-items-center col-6 px-0">
                         <small>calibre</small>
                         <strong>“Victoria”</strong>
@@ -22,7 +22,7 @@ class Ostras extends React.Component{
                           <strong className="col mx-0 px-1">30</strong><p className="col mx-0 px-1"><small>Unidades</small></p><strong className="col mx-0 px-1">21.000</strong>
                         </div>
                     </div>
-               </div>
+               </div> */}
                
                <div className="row mt-3 d-flex justify-content-between">
                     <div className="d-flex flex-column align-items-center col-6 px-0">
