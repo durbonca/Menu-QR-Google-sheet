5e68428c05b7e94dfc5386eccadfc862534ad63e
Manuel Duran <durbonca@gmail.com>
2021-05-14 11:21:22 -0400

    setting loading component at the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,37 +1,42 @@
 import React, {useState, useEffect} from "react"
 import { sanitizedFatherCategories } from '../utils/bussiness'
 import {Link} from "react-router-dom"
-
+import Loading from '../components/loading/'
 function Home (){
 
     const [items, setItems] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
 
     useEffect(() => {
-        fetch("https://bc-backend-prod.herokuapp.com/product-categories/?rest.rest_id_eq=bc-qr&parent_category_null=true")
+        fetch("https://bc-backend-prod.herokuapp.com/product-categories/?rest.rest_id_eq=bc-qr")
           .then(res => res.json())
           .then(
             (result) => {
               const sanitizedCat = sanitizedFatherCategories(
                   result.map(obj => obj)
                 )
+                setIsLoaded(true)
                 setItems(sanitizedCat)
               });
       }
       ,[])
 
-        return(
+        return( 
+
             <div className="vh-100">
                 <img src={process.env.PUBLIC_URL + "logo-baco.svg"} alt="logo" className="col-10 h-50"/>
-                
+                {
+                !isLoaded? <Loading/> :
                 <div className="navLinks">
                 {items.map((menuItem) => {
-                    return (
-                        <Link key={menuItem.slug} className="col-8 d-flex justify-content-center align-items-center btn btn-data rounded" to={`/menu/${menuItem.title}`}>
-                            <div><p><strong>{menuItem.title}</strong></p></div>
-                        </Link>
+                        return (
+                            <Link key={menuItem.slug} className="col-8 d-flex justify-content-center align-items-center btn btn-data rounded" to={`/menu/${menuItem.title}`}>
+                                <div><p><strong>{menuItem.title}</strong></p></div>
+                            </Link>
+                        )}
                     )}
-                )}
                 </div>
+                }
             </div> 
         )   
 }
