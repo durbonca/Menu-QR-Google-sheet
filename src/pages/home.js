c15216af2de0faa5ea418fa9e1c08777936d4e02
Manuel Duran <durbonca@gmail.com>
2021-05-14 11:00:28 -0400

    sanitaze father cat

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react"
-import { sanitizedCategories } from '../utils/bussiness'
+import { sanitizedFatherCategories } from '../utils/bussiness'
 import {Link} from "react-router-dom"
 
 function Home (){
@@ -7,11 +7,11 @@ function Home (){
     const [items, setItems] = useState([])
 
     useEffect(() => {
-        fetch("https://bc-backend-prod.herokuapp.com/product-categories/?rest.rest_id_eq=bc-qr")
+        fetch("https://bc-backend-prod.herokuapp.com/product-categories/?rest.rest_id_eq=bc-qr&parent_category_null=true")
           .then(res => res.json())
           .then(
             (result) => {
-              const sanitizedCat = sanitizedCategories(
+              const sanitizedCat = sanitizedFatherCategories(
                   result.map(obj => obj)
                 )
                 setItems(sanitizedCat)
--- src/utils/bussiness.js
+++ src/utils/bussiness.js
@@ -17,7 +17,41 @@ const transformStringToObject = (str: string) => {
     return productPriorities
   }
 
-const sanitizedCategories = allCat => {
+export const sanitizedFatherCategories = allCat => {
+  const sortedProducts = allCat.map(cat => {
+    let product_priorities = transformStringToObject(cat.product_priorities)
+    
+    if (product_priorities === null) product_priorities = {}
+
+    const sortedProductArray = cat.products.sort((a, b) => {
+      let tmp_a = product_priorities[a.product_id]
+      let tmp_b = product_priorities[b.product_id]
+      if (tmp_a === undefined) tmp_a = 0
+      if (tmp_b === undefined) tmp_b = 0
+      return tmp_b - tmp_a
+    }, product_priorities)
+    return { ...cat, products: sortedProductArray, product_priorities }
+  })
+
+  const sortedCat = sortedProducts.sort((a, b) => {
+    return b.priority - a.priority
+  })
+
+  const allParents = sortedCat.filter(category => {
+    if (category.parent_category === null) {
+      // replace child_cats for the one in sorted (with products)
+      // eslint-disable-next-line
+      category.child_categories = sortedCat.filter(cat => {
+        if (cat.parent_category && cat.parent_category.slug === category.slug)
+          return cat
+      })
+      return category
+    } else return null
+  })
+  return allParents
+}
+
+export const sanitizedCategories = allCat => {
     const sortedProducts = allCat.map(cat => {
       let product_priorities = transformStringToObject(cat.product_priorities)
       
@@ -50,7 +84,3 @@ const sanitizedCategories = allCat => {
     })
     return allParents
   }
-
-export {
-    sanitizedCategories
-}
