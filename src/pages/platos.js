77fb111927c872dd092c67b32f76b38434e9ae89
hirebolledo <durbonca@gmail.com>
2021-05-28 15:47:46 -0400

    adding KEY to items

---
+++ src/pages/platos.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import { FamiliaPlatos, Loading } from '../components'
+import { sanitizedCategories } from '../utils/bussiness'
+import { Link } from 'react-router-dom'
+
+export default function Platos() {
+
+    const [items, setItems] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        const GSheetReader = require('g-sheets-api');
+        const options = {
+          sheetId: '105CwjCT4ocNcCZXeTzmWejPGpG_JOX3moE-aVixp2v4',
+          sheetNumber: 1,
+          returnAllResults: true,
+          filter: {},
+          filterOptions: {}
+        }
+        
+        GSheetReader(
+          options,
+          results => {
+            const sanitizedItems = sanitizedCategories(
+                  results.map(obj => obj)
+                )
+            setItems(sanitizedItems)
+            setIsLoaded(true)
+          },
+          error => {
+            console.error(error)
+          })
+      }
+      ,[])
+    
+    return (
+        <div>
+            {isLoaded ?
+            <div className="container-fluid offset-md-3 col-md-6 mt-5 mb-5 px-4">
+            <Link className="btn backButtom" to="/home"><span className="emoji" role="img" aria-label={""}>🔙</span></Link>
+                {
+                    items.map((props, index)=> <FamiliaPlatos key={index} {...props}/>)
+                }
+            </div>    
+            :
+            <Loading/>
+            }
+        </div>
+    )
+}
